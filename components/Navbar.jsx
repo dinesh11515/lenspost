d3e9291e9b9ca598a1a1fd92f6316734491cd615
dinesh11515 <dineshaitham2@gmail.com>
2022-11-06 22:49:58 +0530

    added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,12 +9,8 @@ export default function Navbar() {
                <p>LensPost</p>
             </div>
             <div className="flex items-center gap-20 text-lg ">
-                <Link href="/">
-                    <button>Post</button>
-                </Link>
-                <Link href="/profile">
-                    <button>Profile</button>
-                </Link>
+                
+                
                 {
                     connected ? 
                     token !== undefined && handle? <button className="bg-[#abfe2ccc] px-5 py-2 rounded-full">{handle}</button> 
